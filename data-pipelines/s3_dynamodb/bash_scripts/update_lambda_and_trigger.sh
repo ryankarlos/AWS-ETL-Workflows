efe850c170ab0808c5560399499c5547b34426bf
Ryan Nazareth <ryankarlos@gmail.com>
2022-04-19 21:17:45 +0100

    add bash script

--- data-pipelines/s3_dynamodb/bash_scripts/update_lambda_and_trigger.sh
+++ data-pipelines/s3_dynamodb/bash_scripts/update_lambda_and_trigger.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
+printf '\n Running s3 to dynamo batch write script'
 
-cd ~/Documents/aws_etl/s3_dynamodb
-echo ''
-echo "updating zip file with updated handler"
-zip batch_write_dynamodb.zip ddb_io.py lambda_function.py schema.py
-echo ''
-echo "update function code with updated zip source code"
-aws lambda update-function-code --function-name batch_write_s3_dynamodb --zip-file fileb://batch_write_dynamodb.zip
-echo ''
-echo "put object in S3 to trigger lambda execution"
-aws s3api put-object --bucket movies-data-json --key transformed/moviedata.json --body ../datasets/moviedata.json
+DATA=${1-datasets/moviedata.json}
+FUNCTION_NAME=${2-batch_write_s3_dynamodb}
+S3_BUCKET=${3-movies-data-json}
+OBJECT_KEY=${4-transformed/moviedata.json}
+# use default values if not passed in via command args
+ZIP_PATH=data-pipelines/s3_dynamodb/lambdas/batch_write_dynamodb.zip
+DYNAMO_LAMBDA_PATH=lambda_packages/batch_write_dynamodb
+CMD_ZIP="zip $ZIP_PATH ${DYNAMO_LAMBDA_PATH}/ddb_io.py ${DYNAMO_LAMBDA_PATH}/lambda_function.py ${DYNAMO_LAMBDA_PATH}/schema.py"
+CMD_UPDATE_LAMBDA="aws lambda update-function-code --function-name ${FUNCTION_NAME} --zip-file fileb://${ZIP_PATH}"
+CMD_S3_PUT="aws s3api put-object --bucket ${S3_BUCKET} --key ${OBJECT_KEY} --body ${DATA}"
+
+if [[ $PWD = */aws_etl ]]; then
+  printf  '\n updating zip file with updated handler'
+  echo "running command....:"
+  echo " ${CMD_ZIP}"
+  eval "$CMD_ZIP"
+else
+  echo "current dir in ${PWD}, you need to cd into root of aws_etl repo"
+  exit 1
+fi;
+
+if [[ -$? -eq 0 ]];then
+  printf '\n update function code with updated zip source code \n'
+  eval "$CMD_UPDATE_LAMBDA"
+fi;
+
+if [[ -$? -eq 0 ]]; then
+  printf '\n put object in S3 to trigger lambda execution \n'
+  eval "$CMD_S3_PUT"
+fi;
